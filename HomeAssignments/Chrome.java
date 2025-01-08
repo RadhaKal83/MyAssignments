d89c0c46be9b64cfb865f74515e17a1fd67585ac
Radha K <radhakalidass@gmail.com>
2025-01-08 19:46:47 +0200

    Add files via upload
     first home assignments

---
+++ HomeAssignments/Chrome.java
@@ -0,0 +1,15 @@
+package week1.HomeAssignments;
+
+public class Chrome {
+
+	public static void main(String[] args)
+	{
+      float version = 91.0f;
+      String name = "Google";
+      boolean isBeta = false;
+      int yearType = 2008;
+      char shortcutKey ='C';
+      System.out.println("Version is :" + version + "Name is :"+ name + isBeta + yearType + shortcutKey );
+	}
+
+}
